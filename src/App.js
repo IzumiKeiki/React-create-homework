9e5e15374674f245b602d8e4dc7afe517066d47e
Nhat Khanh <keiki@Anns-MacBook-Air.local>
2023-06-07 17:18:04 +0900

    apply style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ function App() {
   return (
     <>
       <h1>Hello World !</h1>
+      <p className="class">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum harum
+        corrupti beatae eius ex facere, optio perspiciatis eaque dolores nobis
+        officiis nesciunt et incidunt unde ab, culpa excepturi deserunt aliquid?
+      </p>
+      <p id="id">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, rem
+        molestiae perferendis itaque recusandae facere perspiciatis veritatis
+        deleniti odit iste cupiditate maiores magnam, maxime esse? Tempore magni
+        quis ut quisquam!
+      </p>
+      <p style={{ color: "green" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+        corrupti cum, error hic modi velit nisi facere, sapiente culpa fuga
+        necessitatibus inventore in omnis ipsa porro incidunt iure, animi nemo!
+      </p>
     </>
   );
 }
