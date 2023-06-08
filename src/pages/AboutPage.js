5fbd0a56e34ddc8c8a153cf75c9172e72addb8f8
Nhat Khanh <keiki@Anns-MacBook-Air.local>
2023-06-08 12:54:41 +0900

    create layout

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,17 @@
+function AboutPage() {
+  return (
+    <>
+      <div className="aboutpage">
+        <h1>About us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+          possimus voluptates consectetur dolor magni soluta minus omnis facere
+          voluptate vero hic labore quaerat eaque sint corporis voluptatum qui
+          tenetur earum.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default AboutPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import Main from "../layout/Main";
+import Sidebar from "../layout/Sidebar";
+
+function HomePage() {
+  return (
+    <>
+      <div className="homepage">
+        <Main />
+        <Sidebar />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
